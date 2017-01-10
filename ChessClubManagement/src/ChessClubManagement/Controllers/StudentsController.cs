830c2a04419db6d0d8d08f2b8416d4e30edd70f9
Robert McCoy <rmccoy6435@gmail.com>
2017-01-10 11:36:18 -0500

    Added in first View and Controller for Students and modified the layout and home page.

---
+++ ChessClubManagement/src/ChessClubManagement/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using ChessClubManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChessClubManagement.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly StudentRepository _repository;
+        public StudentsController(ChessClubContext context)
+        {
+            _repository = new StudentRepository(context);
+        }
+
+        public IActionResult Index()
+        {
+            return View(_repository.GetStudents());
+        }
+    }
+}
---
+++ ChessClubManagement/src/ChessClubManagement/Models/StudentRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChessClubManagement.Models
+{
+    public class StudentRepository
+    {
+        private readonly ChessClubContext _context;
+
+        public StudentRepository(ChessClubContext context)
+        {
+            _context = context;
+        }
+
+        public List<Students> GetStudents()
+        {
+            return _context.Students.ToList();
+        }
+    }
+}
